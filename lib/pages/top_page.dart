3f3c86769d7dc817fa61f4116782750552d24c0d
KobayashiYoh <y.kobayashi@4nonome.com>
2023-06-25 12:52:06 +0900

    TopPageを作成

---
+++ lib/pages/top_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TopPage extends StatelessWidget {
+  const TopPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Top Page'),
+      ),
+    );
+  }
+}
