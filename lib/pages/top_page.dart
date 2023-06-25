1331f0c1f659d0bf2440777bb8b07296973c7f39
KobayashiYoh <y.kobayashi@4nonome.com>
2023-06-25 12:55:42 +0900

    画面遷移を追加

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_mini_hackathon_team_b/pages/home_page.dart';
 
 class TopPage extends StatelessWidget {
   const TopPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Top Page'),
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => const HomePage()),
+            );
+          },
+          child: const Text('HomePageへ遷移'),
+        ),
       ),
     );
   }
