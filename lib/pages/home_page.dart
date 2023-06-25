561ab6d45fe57a990a31cdb1278b63c21d2b01de
KobayashiYoh <y.kobayashi@4nonome.com>
2023-06-25 12:58:23 +0900

    アラートダイアログ表示ボタンを追加

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,9 +5,14 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Home Page'),
+        child: ElevatedButton(
+          onPressed: () {
+            // TODO: アラートダイアログを表示
+          },
+          child: const Text('アラートダイアログを表示'),
+        ),
       ),
     );
   }
